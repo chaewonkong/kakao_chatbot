ff405aec7df9c23dde4f28c422e427c3307a778c
공채원 <chaewonkong@gmail.com>
2018-03-09 14:13:43 +0900

    Insert get_stock_index() inside of JsonResponse return obj

--- buttons/views.py
+++ buttons/views.py
@@ -17,12 +17,11 @@ def message(request):
 	json_str = ((request.body).decode('utf-8'))
 	json_data = json.loads(json_str)
 	market_name = json_data['content']
-	index = get_stock_index(market_name)
 
 
 	return JsonResponse({
 		'message': {
-			'text': market_name + '의' + '지수입니다: ' + index
+			'text': market_name + '의' + '지수입니다: \n\n' + get_stock_index(market_name)
 			},
 		'keyboard': {
 			'type': 'buttons',
