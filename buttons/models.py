bfca2557d32586f092c39a52536f14a670468171
공채원 <chaewonkong@gmail.com>
2018-03-09 17:45:07 +0900

    Add id to models again

--- buttons/models.py
+++ buttons/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Index(models.Model):
 	"""Store KOSPI and KOSDAQ Index"""
-
+	id = models.AutoField(primary_key=True)
 	market_name = models.CharField(max_length=30, default="")
 	index = models.CharField(max_length=50, default="")
 
