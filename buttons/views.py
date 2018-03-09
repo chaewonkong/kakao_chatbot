fc34c765f1aee6b562f8520dc4bcc33d016d773b
공채원 <chaewonkong@gmail.com>
2018-03-09 16:42:26 +0900

    Add time.sleep(3) to scraper

--- buttons/views.py
+++ buttons/views.py
@@ -2,6 +2,7 @@
 from apis.scraper import get_stock_index
 from bs4 import BeautifulSoup
 import requests
+import time
 
 # Django App
 from buttons.models import Index
@@ -44,6 +45,7 @@ def scraper(request):
 
 	create_index('코스피', get_stock_index('코스피'))
 	create_index('코스닥', get_stock_index('코스닥'))
+	time.sleep(3)
 
 	return HttpResponse("크롤링이 진행 중입니다~!!")
 
