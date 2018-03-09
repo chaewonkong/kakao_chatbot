272f2e7041d5f6a8a660a06fe32e99776d5bf135
공채원 <chaewonkong@gmail.com>
2018-03-09 14:28:31 +0900

    Import bs4, requests in views

--- buttons/views.py
+++ buttons/views.py
@@ -1,4 +1,9 @@
+# Web scraper
 from apis.scraper import get_stock_index
+from bs4 import BeautifulSoup
+import requests
+
+# Django App
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -17,11 +22,12 @@ def message(request):
 	json_str = ((request.body).decode('utf-8'))
 	json_data = json.loads(json_str)
 	market_name = json_data['content']
+	index = get_stock_index(market_name)
 
 
 	return JsonResponse({
 		'message': {
-			'text': market_name + '의' + '지수입니다: \n\n' + get_stock_index(market_name)
+			'text': market_name + '의' + '지수입니다: \n\n' + index
 			},
 		'keyboard': {
 			'type': 'buttons',
