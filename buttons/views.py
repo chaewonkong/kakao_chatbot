6b73fc56a761113f53bf757240e8bf52a7ba0dd6
공채원 <chaewonkong@gmail.com>
2018-03-10 13:35:19 +0900

    Add 'won' to return stock price msg

--- buttons/views.py
+++ buttons/views.py
@@ -48,7 +48,7 @@ def message(request):
 	else: # Return specific stock price and keyboard to users
 		return JsonResponse({
 			'message': {
-				'text': action + '의 현재가(종가) 입니다:\n\n' + index[0]
+				'text': action + '의 현재가(종가) 입니다:\n\n' + index[0] + ' 원(KRW)'
 				},
 			'keyboard':{
 				'type': 'buttons',
