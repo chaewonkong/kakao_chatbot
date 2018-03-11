fc96b40810391ad5c9d2c220793d71bc1b137a2e
공채원 <chaewonkong@gmail.com>
2018-03-11 20:33:53 +0900

    Change comments in models

--- buttons/models.py
+++ buttons/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Index(models.Model):
-	"""Store KOSPI and KOSDAQ Index"""
+	"""Store KOSPI and KOSDAQ Index to DB"""
+
 	id = models.AutoField(primary_key=True)
 	market_name = models.CharField(max_length=30, default="")
 	index = models.CharField(max_length=50, default="")
@@ -12,6 +13,8 @@ class Index(models.Model):
 
 
 class Code(models.Model):
+	"""Store corporate name matched with stock code to DB"""
+
 	id = models.AutoField(primary_key=True)
 	corp_name = models.CharField(max_length=40, default="")
 	corp_code = models.CharField(max_length=30, default="")
