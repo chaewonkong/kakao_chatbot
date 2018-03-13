1ee0451da6dea8351211dff4f68cc4962e58a31a
공채원 <chaewonkong@gmail.com>
2018-03-13 11:24:20 +0900

    Substract code_scraper to a new py file

---
+++ apis/code_scraper.py
@@ -0,0 +1,25 @@
+"""Save code from csv to DB"""
+from buttons.models import Code
+
+
+def corp_code_scraper():
+	code_db = Code.objects.all()
+	code_db.delete()
+
+	create_code()
+
+
+def create_code():
+
+	with open('apis/data.csv') as datafile:
+		reader = csv.reader(datafile)
+
+		for row in reader:
+			Code.objects.create(
+				corp_name = row[1],
+				corp_code = row[0]
+				)
+
+
+# Update corporate code to DB
+corp_code_scraper()
--- buttons/views.py
+++ buttons/views.py
@@ -60,25 +60,6 @@ def message(request):
 		return JsonResponse(post_stock_price(action))
 
 
-def corp_code_scraper():
-	code_db = Code.objects.all()
-	code_db.delete()
-
-	create_code()
-
-
-def create_code():
-
-	with open('apis/data.csv') as datafile:
-		reader = csv.reader(datafile)
-
-		for row in reader:
-			Code.objects.create(
-				corp_name = row[1],
-				corp_code = row[0]
-				)
-
-
 def get_index():
 	"""Return index of given market_name from DB"""
 
